94095f45cca3d9eba8245903ffa55d23a3b10091
Mattiarey <102354699+Mattiarey@users.noreply.github.com>
2024-05-13 19:10:40 +0200

    fatto css bello per i tastini

--- View/aggiungiGita.php
+++ View/aggiungiGita.php
@@ -8,11 +8,14 @@
     <!--Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
     <!--Fine font-->
     <title>Aggiungi Gita</title>
@@ -25,12 +28,15 @@
             <img src="../View/images/userBianco.png" alt="immagine utente" id="userImage">
         </div>
         <h1 id="titolo">Aggiungi Gita</h1>
-        <a href="./homepage.html"><input type="submit" value="Torna indietro"></a>
+        <div class="tastini">
+            <a href="./homepage.html"><input type="submit" value="Torna indietro"></a>
+        </div>
     </div>
     <div class="corpo">
         <form action="../index.php/aggiungiGita" method="POST" class="contenitore">
             <div class="inserimento"><input type="text" placeholder="Nome" maxlength="20" name="nome"></div>
-            <div class="inserimento"><input type="textarea" placeholder="Descrizione" maxlength="255" name="descrizione"></div>
+            <div class="inserimento"><input type="textarea" placeholder="Descrizione" maxlength="255"
+                    name="descrizione"></div>
             <div class="inserimento"><input type="date" placeholder="Data" name="data"></div>
             <div class="inserimento"><input type="number" placeholder="Costo" name="costo"></div>
             <div class="inserimento"><input type="submit" value="Invia"></div>
--- View/aggiungiMeta.php
+++ View/aggiungiMeta.php
@@ -8,11 +8,14 @@
     <!--Fonts-->
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css2?family=Alegreya+Sans:ital,wght@0,100;0,300;0,400;0,500;0,700;0,800;0,900;1,100;1,300;1,400;1,500;1,700;1,800;1,900&family=Exo+2:ital,wght@0,100..900;1,100..900&display=swap"
+        rel="stylesheet">
 
     <!--Fine font-->
     <title>Aggiungi Meta</title>
@@ -25,12 +28,16 @@
             <img src="../View/images/userBianco.png" alt="immagine utente" id="userImage">
         </div>
         <h1 id="titolo">Aggiungi Meta</h1>
-        <a href="./homepage.html"><input type="submit" value="Torna indietro"></a>
+        <div class="tastini">
+            <a href="./homepage.html"><input type="submit" value="Torna indietro"></a>
+        </div>
     </div>
     <div class="corpo">
-        <form action="../index.php/aggiungiMeta?mail=<?php echo $_COOKIE['UserConnesso'] ?>"method="POST" class="contenitore">
+        <form action="../index.php/aggiungiMeta?mail=<?php echo $_COOKIE['UserConnesso'] ?>" method="POST"
+            class="contenitore">
             <div class="inserimento"><input type="text" placeholder="Nome" maxlength="20" name="nome"></div>
-            <div class="inserimento"><input type="textarea" placeholder="Descrizione" maxlength="255" name="descrizione"></div>
+            <div class="inserimento"><input type="textarea" placeholder="Descrizione" maxlength="255"
+                    name="descrizione"></div>
             <div class="inserimento"><input type="number" placeholder="Durata" name="durata"></div>
             <div class="inserimento"><input type="number" placeholder="Costo" name="costo"></div>
             <div class="inserimento"><input type="text" placeholder="Nome Gita" name="fkMeta"></div>
